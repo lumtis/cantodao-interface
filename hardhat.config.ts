9e42b76d1152843d806fc2a9089322706d9dfb2f
lubtd <lucas.bertrand.22@gmail.com>
2023-02-13 12:03:14 +0100

    chore: set chain id for local testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,11 @@ import "@nomicfoundation/hardhat-toolbox";
 import { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
   solidity: {
     version: "0.8.17",
     settings: {
