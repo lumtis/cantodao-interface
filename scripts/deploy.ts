44bf36f38052262a99e2a129b8a80a74fe7d5486
lubtd <lucas.bertrand.22@gmail.com>
2023-02-15 19:04:56 +0100

    chore: scripts for localnet interactions

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -213,8 +213,6 @@ const main = async () => {
   );
 };
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
