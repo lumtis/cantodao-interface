482e44c5904218d8b569b3f3e8d62fe4317f66d7
lubtd <lucas.bertrand.22@gmail.com>
2023-02-16 09:47:40 +0100

    refactor: improve home page

--- interface/pages/index.tsx
+++ interface/pages/index.tsx
@@ -1,17 +1,14 @@
-import Head from 'next/head';
+import Head from "next/head";
 
-import {
-  Box,
-  Heading,
-  Image,
-  Text,
-} from '@chakra-ui/react';
+import { Box, Heading, Image, Link, Text } from "@chakra-ui/react";
 
-import ContainerPage from '../components/ui/container-page';
-import Layout from '../layout/Layout';
+import ContainerPage from "../components/ui/container-page";
+import Layout from "../layout/Layout";
 
-const WelcomeText =
+const welcomeText =
   "Cantodao is a DAO system built on Canto network. Cantodao is fully open-source, impregnated with the freedom culture of Canto, and is aimed to become the simplest and easiest-to-use DAO system.";
+
+const arrow = "->";
 
 const HomePage = () => {
   return (
@@ -26,17 +23,40 @@ const HomePage = () => {
           <Image m="auto" src="/static/images/man.png" maxW={20} maxH={20} />
           <Heading>Welcome to cantodao</Heading>
           <Box display="flex" flexDirection="row" alignItems="center">
-            <Text textAlign="left" fontSize="30px" m="auto" mt="20" mb="20" w={600}>
-              {WelcomeText}
+            <Text
+              textAlign="left"
+              fontSize="30px"
+              m="auto"
+              mt="20"
+              mb="20"
+              w={600}
+            >
+              {welcomeText}
             </Text>
             <Image m="auto" src="/static/images/space.png" w={500} />
           </Box>
           <Box display="flex" flexDirection="row" alignItems="flex-end">
-            <Image m="auto" src="/static/images/beach.png" w={400} />
-            <Text textAlign="left" fontSize="30px" ml="20px" mt="20" mb="20" w={600}>
-              -> Explore the DAOs
-              -> Create your DAO
-            </Text>
+            <Image m="auto" src="/static/images/beach.png" w={300} />
+            <Box ml="40px" mt="20" mb="20" w={600}>
+              <Text textAlign="left" fontSize="40px">
+                Getting started
+              </Text>
+              <Link href="/explore">
+                <Text textAlign="left" fontSize="30px">
+                  {arrow} Explore the listed DAOs
+                </Text>
+              </Link>
+              <Link href="/create">
+                <Text textAlign="left" fontSize="30px">
+                  {arrow} Create your own DAO
+                </Text>
+              </Link>
+              <Link href="https://github.com/lumtis/cantodao">
+                <Text textAlign="left" fontSize="30px">
+                  {arrow} Check the source code
+                </Text>
+              </Link>
+            </Box>
           </Box>
         </Box>
       </ContainerPage>
