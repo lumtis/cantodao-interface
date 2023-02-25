f9d8a0a50986e81a147c8a10bae1b348f78324b8
lubtd <lucas.bertrand.22@gmail.com>
2023-02-25 15:52:31 +0100

    chore: setup front-end codebase

--- contracts/interfaces/IDAOProposer.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: APACHE-2.0
-pragma solidity ^0.8.9;
-
-import "../DAOGovernor.sol";
-
-interface IDAOProposer {
-    function setGovernor(DAOGovernor _governor) external;
-}
--- contracts/interfaces/IDAOProposerDeployer.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: APACHE-2.0
-pragma solidity ^0.8.9;
-
-interface IDAOProposerDeployer {
-    function deployDAOProposer() external returns (address);
-}
